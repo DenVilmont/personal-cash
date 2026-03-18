5680506523d672a7cb5d1c2bffa0075837c18cb2
Dmitrii Davydov <denvilmont@gmail.com>
2026-03-18 09:50:58 +0100

    Home page

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using PersonalCash.Shared;
+
+namespace PersonalCash.Pages;
+
+public partial class Index
+{
+    [Inject] private AppPageTitleState PageTitleState { get; set; } = default!;
+
+    protected override void OnParametersSet()
+    {
+        PageTitleState.Clear();
+    }
+}
